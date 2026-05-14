37e2b427d1224c62aa67c40ad4fd529ff3ae3882
yaseumine <yasminmaharani123@gmail.com>
2026-05-14 23:41:36 +0700

    feat : ngambil library
    mengambil library

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import local_auth_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
 }
