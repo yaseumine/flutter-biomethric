d18d4e010ebfaae3546e1e4bfd216bd0393bf448
yaseumine <yasminmaharani123@gmail.com>
2026-05-14 23:43:09 +0700

    chore : ganti build gradle
    ganti

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,44 +1,31 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.biometricauth"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    namespace = "com.example.biometric_auth"
+    compileSdk = flutter.compileSdkVersion   // Gunakan versi dari Flutter SDK
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17   // Java 17 diperlukan oleh local_auth v3
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.biometricauth"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        applicationId = "com.example.biometric_auth"
+        minSdk = flutter.minSdkVersion      // minimum Android 4.3 (API 18)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
 }
 
-flutter {
-    source = "../.."
+dependencies {
+    implementation("androidx.fragment:fragment-ktx:1.6.2")  // ← WAJIB untuk local_auth v3
 }
