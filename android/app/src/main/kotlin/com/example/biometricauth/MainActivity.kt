01b0bf46061a9381bbf3227e034aff3fac13c571
yaseumine <yasminmaharani123@gmail.com>
2026-05-14 23:46:35 +0700

    chore: gati main activity kt

--- android/app/src/main/kotlin/com/example/biometricauth/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/biometricauth/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.biometricauth
+package com.example.biometric_auth // Sesuaikan dengan nama package kamu
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
